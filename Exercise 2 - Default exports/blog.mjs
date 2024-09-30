e5474a25feb8172d239573e67486bd971e871ece
Ashwin <ashwinvx@users.noreply.github.com>
2024-09-30 10:47:24 -0400

    Exercise 2 Named exports

---
+++ Exercise 2 - Default exports/blog.mjs
@@ -0,0 +1,5 @@
+// Create blog.mjs that imports the Post class. Create an instance, and call the publish method to simulate posting.
+import Post from './post.mjs';
+
+const post1 = new Post('Gen AI', 'Generative AI is new.');
+post1.publish();
